fe77812c447c73b4dbfe6d2a8ee0a9882530d120
Geoffrey Goodman <ggoodman@gmail.com>
2015-11-03 14:55:30 -0500

    Add first-class CronJob object

---
+++ test/cron.js
@@ -0,0 +1,99 @@
+require('dotenv').load({ silent: true });
+
+var Bluebird = require('bluebird');
+var Code = require('code');
+var Lab = require('lab');
+var Sandbox = require('../');
+
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+
+var sandboxParams = {
+    url: process.env.SANDBOX_URL,
+    token: process.env.SANDBOX_TOKEN,
+    container: process.env.SANDBOX_CONTAINER,
+};
+
+
+lab.experiment('CronJob', {parallel: false, timeout: 10000}, function () {
+    var googleTestCodeUrl = 'https://cdn.auth0.com/webtasks/test_cron_google.js';
+
+    lab.test('test pre-requisites are met', function (done) {
+
+        expect(sandboxParams.url).to.exist();
+        expect(sandboxParams.token).to.exist();
+        expect(sandboxParams.container).to.exist();
+
+        done();
+    });
+    
+    lab.test('creation will throw if a named webtask is not used', function (done) {
+        var sandbox = Sandbox.init(sandboxParams);
+        var resolved = false;
+
+        sandbox.create(googleTestCodeUrl)
+            .call('createCronJob', '* * * * *')
+            .tap(function (job) {
+                resolved = true;
+            })
+            .catch(function (err) {
+                expect(err).to.be.an.instanceOf(Error);
+                expect(err.message).to.equal('Cron jobs can only be created from named webtasks.');
+            })
+            .finally(function () {
+                expect(resolved).to.equal(false);
+            })
+            .nodeify(done);
+    });
+    
+    lab.test('instances can be created from Webtask instances and can destroy themselves', function (done) {
+        var sandbox = Sandbox.init(sandboxParams);
+        var jobName = 'sandboxjs-test';
+
+        sandbox.create(googleTestCodeUrl, { name: jobName })
+            .call('createCronJob', '* * * * *')
+            .tap(function (job) {
+                expect(job).to.be.an.instanceOf(Sandbox.CronJob);
+                expect(job.cluster_url).to.equal(sandbox.url);
+                expect(job.name).to.equal(jobName);
+            })
+            .tap(function (job) {
+                return job.remove();
+            })
+            .tap(function (job) {
+                return Bluebird.join(job, sandbox.listCronJobs(), function (removed, jobs) {
+                    jobs.forEach(function (job) {
+                        expect(job.name).to.not.equal(removed.name);
+                    });
+                });
+            })
+            .nodeify(done);
+    });
+    
+    lab.test('listings can be retrieved from a Sandbox', function (done) {
+        var sandbox = Sandbox.init(sandboxParams);
+        var jobName = 'sandboxjs-test';
+
+        sandbox.create(googleTestCodeUrl, { name: jobName })
+            .call('createCronJob', '* * * * *')
+            .tap(function (job) {
+                var found = false;
+                
+                expect(job).to.be.an.instanceOf(Sandbox.CronJob);
+                expect(job.cluster_url).to.equal(sandbox.url);
+                expect(job.name).to.equal(jobName);
+                
+                return Bluebird.join(job, sandbox.listCronJobs(), function (created, jobs) {
+                    jobs.forEach(function (job) {
+                        found = found || (job.name === created.name);
+                    });
+                    
+                    expect(found).to.equal(true);
+                });
+            })
+            .tap(function (job) {
+                return job.remove();
+            })
+            .nodeify(done);
+    });
+});
